8700c7be26bc7f39288bb2296a344fce161deae1
Spencer <stoddard.spencer@gmail.com>
2020-12-23 16:24:18 +0000

    Continued implementing PEP-8 coding standards on main.py file.

--- main.py
+++ main.py
@@ -9,112 +9,122 @@ from functions import get_quote, update_encouragements, delete_encouragement
 client = discord.Client()
 
 # List of sad words
-sad_words = ["sad", "depressed", "lonely", "crying", "alone", "unhappy", "miserable", "depressing", "anger", "angry"]
+sad_words = ["sad", "depressed",
+"lonely", "crying", "alone",
+"unhappy", "miserable", "depressing",
+"anger", "angry"]
 
 # List of encouraging messages
 # Reason for variable name: user can add more encouragements to the database
-starter_encouragements = ["Cheer up", "Hang in there", "You are a wonderful soul", "I am here for you"]
+starter_encouragements = ["Cheer up",
+"Hang in there", "You are a wonderful soul",
+"I am here for you"]
 
 # List of greetings
-bot_greetings = ["Hello!", "Hey!", "Greetings and salutations.", "What's up?", "Hi!", "Good to see you again.", "Hey dude!"]
+bot_greetings = ["Hello!", "Hey!",
+"Greetings and salutations.", "What's up?",
+"Hi!", "Good to see you again.",
+"Hey dude!"]
+
 
 # Event for logging in
 @client.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(client))
+
 
 # Event for getting the message
 @client.event
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  # Create new variables for easier access
-  msg = message.content
-  greeting = bot_greetings
-  options = starter_encouragements
-  
-  # If user types $inspire, call the get_quote() function,
-  #then show the quote to the user
-  if msg.startswith('$inspire'):
-    quote = get_quote()
-    await message.channel.send(quote)
+    # Create new variables for easier access
+    msg = message.content
+    greeting = bot_greetings
+    options = starter_encouragements
 
-  # If our message starts with any variation of hi, hey or hello,
-  #then choose a random greeting for the bot to use
-  if msg.lower() == "hi" or msg.lower() == "hey" or msg.lower() == "hello":
-    await message.channel.send(random.choice(greeting))
+    # If user types $inspire, call the get_quote() function,
+    # Then show the quote to the user
+    if msg.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
 
-  # If our message is $rickroll, then rick roll the user
-  if msg.startswith('$rickroll'):
-    link = "https://www.youtube.com/watch?v=dGeEuyG_DIc"
-    await message.channel.send(link)
-    
-  # If "responding" isn't in our database,
-  #then add True
-  if db["responding"]:
-    if "responding" not in db.keys():
-      db["responding"] = True
+    # If our message starts with any variation of hi, hey or hello,
+    # Then choose a random greeting for the bot to use
+    if msg.lower() == "hi" or msg.lower() == "hey" or msg.lower() == "hello":
+      await message.channel.send(random.choice(greeting))
 
-  # If encouragements is in our database,
-  #add the encouragements to the database
-  if "encouragements" in db.keys():
-    options = options + db["encouragements"]
+    # If our message is $rickroll, then rick roll the user
+    if msg.startswith('$rickroll'):
+      link = "https://www.youtube.com/watch?v=dGeEuyG_DIc"
+      await message.channel.send(link)
 
-  # Go through all of the words in the list of 
-  # Sad words, and if any of the words are 
-  # In the message, randomly choose an encouraging 
-  # Phrase for the user to see
-  if any(word in msg for word in sad_words):
-    await message.channel.send(random.choice(options))
+    # If "responding" isn't in our database,
+    # Then add True
+    if db["responding"]:
+      if "responding" not in db.keys():
+        db["responding"] = True
 
-  # If the user types in $new, then our message 
-  # Will be split, and we'll get the message 
-  # As an array
-  if msg.startswith('$new'):
-    encouraging_message = msg.split("$new ",1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("New encouraging message has been added.")
-  
-  # If the user types in $del, then create a new list,
-  # Of encouragements, and if it's in our database,
-  # Delete the encouragement from our database,
-  # Then update the database, and show the new list
-  # Of encouragements
-  if msg.startswith('$del'):
-    encouragements = []
+    # If encouragements is in our database,
+    # Add the encouragements to the database
     if "encouragements" in db.keys():
-      index = int(msg.split('$del',1)[1])
-      delete_encouragement(index)
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
+      options = options + db["encouragements"]
 
-  # If the user types in $list, thehn create a new list,
-  # And if the encouragements are in our database,
-  # Show the user the list of encouragements
-  if msg.startswith('$list'):
-    encouragements = []
-    if "encouragements" in db.keys():
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
+    # Go through all of the words in the list of
+    # Sad words, and if any of the words are
+    # In the message, randomly choose an encouraging
+    # Phrase for the user to see
+    if any(word in msg for word in sad_words):
+      await message.channel.send(random.choice(options))
 
-  # If the user types in $responding, then take the
-  # User input, and if they type true, notify the user
-  # That responding is on. If they type false, notify
-  # The user that responding is off.
-  if msg.startswith('$responding'):
-    value = msg.split("$responding ",1)[1]
+    # If the user types in $new, then our message
+    # Will be split, and we'll get the message
+    # As an array
+    if msg.startswith('$new'):
+      encouraging_message = msg.split("$new ", 1)[1]
+      update_encouragements(encouraging_message)
+      await message.channel.send("New encouraging message has been added.")
+
+    # If the user types in $del, then create a new list,
+    # Of encouragements, and if it's in our database,
+    # Delete the encouragement from our database,
+    # Then update the database, and show the new list
+    # Of encouragements
+    if msg.startswith('$del'):
+      encouragements = []
+      if "encouragements" in db.keys():
+        index = int(msg.split('$del', 1)[1])
+        delete_encouragement(index)
+        encouragements = db["encouragements"]
+      await message.channel.send(encouragements)
+
+    # If the user types in $list, thehn create a new list,
+    # And if the encouragements are in our database,
+    # Show the user the list of encouragements
+    if msg.startswith('$list'):
+      encouragements = []
+      if "encouragements" in db.keys():
+        encouragements = db["encouragements"]
+      await message.channel.send(encouragements)
+
+    # If the user types in $responding, then take the
+    # User input, and if they type true, notify the user
+    # That responding is on. If they type false, notify
+    # The user that responding is off.
+    if msg.startswith('$responding'):
+      value = msg.split("$responding ", 1)[1]
 
     if value.lower() == "true":
       db["responding"] = True
       await message.channel.send("Responding is on.")
-      
+
     else:
       db["responding"] = False
       await message.channel.send("Responding is off.")
 
-# Runs our web server
-keep_alive()
+    # Runs our web server
+    keep_alive()
 
-# Runs our bot
-client.run(os.getenv('TOKEN'))
+    # Runs our bot
+    client.run(os.getenv('TOKEN'))
